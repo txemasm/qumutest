3a800d42ef948d0f1a0cff8991af0b798d93af2e
txemasm <37071292+txemasm@users.noreply.github.com>
2020-06-29 16:24:25 +0200

    Update pushPageviewToDataLayer.js

--- pushPageviewToDataLayer.js
+++ pushPageviewToDataLayer.js
@@ -1,11 +1,11 @@
 function pushPageviewToDataLayer(currentStep) {
   window.dataLayer.push({
-    event: 'imPageview',
-    context: setContextData(),
-    user: setUserData(), 
-    page: setPageData(currentStep), 
-    deposit: setDepositData(), 
-    ticket: setTicketData(),
-    ecommerce: setEcommerce()
+    'event': 'imPageview',
+    'context': setContextData(),
+    'user': setUserData(), 
+    'page': setPageData(currentStep), 
+    'deposit': setDepositData(), 
+    'ticket': setTicketData(),
+    'ecommerce': setEcommerce()
     });
  }
