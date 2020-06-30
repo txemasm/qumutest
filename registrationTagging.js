4f7af570af8f1d0b014405297e1d62e363f30cad
txemasm <37071292+txemasm@users.noreply.github.com>
2020-06-30 08:29:38 +0200

    Update registrationTagging.js

--- registrationTagging.js
+++ registrationTagging.js
@@ -1,4 +1,15 @@
 function registrationTagging() {
     var currentStep = document.querySelector('.stepwise-registration').getAttribute('data-current-step');
     pushPageviewToDataLayer(currentStep);
-};
+}
+
+function registrationEventTagging() {
+    var currentStep = document.querySelector('.stepwise-registration').getAttribute('data-current-step');
+    var nextStepButton = document.querySelectorAll("input[data-uat='registration-next-button']");
+    clickerFn = function() {
+        pushEventToDataLayer(currentStep);
+    }
+    for (var i=0; i < nextStepButton.length; i++) {
+        nextStepButton.item(i).onclick = clickerFn;
+    }
+}
