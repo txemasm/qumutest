49da4055615dd6c2b59d89bdb8ec0d2d3fde91cd
Txema Sánchez <josesm@improvingmetrics.com>
2020-08-19 10:00:33 +0200

    deep_changes_and_debugger

--- qumulibrary.js
+++ qumulibrary.js
@@ -1,140 +1,212 @@
-(function() {
-  var action, label, videoInfo, imPercents;
+(function () {
+  var qumuTimer = setInterval(qumuTagging, 500);
+  var tryCounter = 0;
+  function qumuTagging() {
+      if(window.jQuery) {
+          clearInterval(qumuTimer);
+          consoleDebugger('start tagging');
+          var action, label, videoInfo, videos = {};
+          var ACTIONS = {
+              'play': 'play',
+              'pause': 'pause',
+              'begin': 'begin',
+              'ended': 'complete',
+              'milestone_passed': 'milestone passed',
+              'minutes_watching': 'minutes watching',
+              'qumu': 'qumu',
+              'media': 'media'
+          }
+          var DEBUGGER = {
+              'bind': ' event binded for video: ',
+              'push': ' event pushed to dataLayer for video: ',
+              'bind_push': ' event binded and pushed to dataLayer for video: '
+          }
 
-  window.widgets.forEach(function(widget) {
-    var api = widget.api;
-    api.init(function(err) {
-      if (err) {
-        return;
+          window.widgets.forEach(function (widget) {
+              var api = widget.api;
+              api.init(function (err) {
+                  if (err) {
+                      return;
+                  }
+
+                  api.get('kulus', function (kulus) {
+                      videoInfo = kulus[0];
+                      initVideosQumu(videoInfo);
+                  });
+
+                  api.bind('load', function () {
+                      getCurrentVideo();
+                      action = getAction('load');
+                      pushToDataLayer(action, label);
+                      consoleDebugger('load' + DEBUGGER.bind_push + videoInfo.guid);
+                  });
+
+                  api.bind('play', function () {
+                      getCurrentVideo();
+                      action = getAction('play');
+                      consoleDebugger('play' + DEBUGGER.bind + videoInfo.guid);
+                      if (videoInfo.currentTime > 500) {
+                          pushToDataLayer(action, label);
+                          consoleDebugger('play' + DEBUGGER.push + videoInfo.guid);
+                      }
+                  });
+
+                  api.bind('pause', function () {
+                      getCurrentVideo();
+                      action = getAction('pause');
+                      consoleDebugger('pause' + DEBUGGER.bind + videoInfo.guid);
+                      var timeToFinish = videoInfo.duration - videoInfo.currentTime;
+                      if (timeToFinish > 500) {
+                          pushToDataLayer(action, label);
+                          consoleDebugger('pause' + DEBUGGER.push + videoInfo.guid);
+                      }
+                  });
+
+                  api.bind('ended', function () {
+                      getCurrentVideo();
+                      action = getAction('ended');
+                      pushToDataLayer(action, label);
+                      consoleDebugger('ended' + DEBUGGER.bind_push + videoInfo.guid);
+                  });
+
+                  api.bind('unload', function () {
+                      getCurrentVideo();
+                      var time = formatTime(videoInfo.currentTime);
+                      action = getAction('unload', time);
+                      pushToDataLayer(action, label);
+                      consoleDebugger('unload' + DEBUGGER.bind_push + videoInfo.guid);
+                  });
+
+                  api.bind('timeupdate', function (currentTime) {
+                      getCurrentVideo();
+                      var time = parseInt(currentTime);
+                      videoInfo.currentTime = currentTime;
+                      var percents = videos[videoInfo.guid].percents;
+                      if (videoInfo.isLiveStream != true) {
+                          if (percents.quarter.hit && (time > percents.quarter.value && time < percents.half.value)) {
+                              percents.quarter.hit = false;
+                              action = getAction('timeupdate', '25%');
+                              pushToDataLayer(action, label);
+                              consoleDebugger('25%' + DEBUGGER.push + videoInfo.guid);
+                          }
+                          if (percents.half.hit && (time > percents.half.value && time < percents.toQuarter.value)) {
+                              percents.half.hit = false;
+                              action = getAction('timeupdate', '50%');
+                              pushToDataLayer(action, label);
+                              consoleDebugger('50%' + DEBUGGER.push + videoInfo.guid);
+                          }
+                          if (percents.toQuarter.hit && (time > percents.toQuarter.value && time < percents.total.value)) {
+                              percents.toQuarter.hit = false;
+                              action = getAction('timeupdate', '75%');
+                              pushToDataLayer(action, label);
+                              consoleDebugger('75%' + DEBUGGER.push + videoInfo.guid);
+                          }
+                      } else {
+                          var remainder = time % 60000;
+                          if (time > 500 && remainder < 250) {
+                              var formattedTime = formatTime(videoInfo.currentTime);
+                              action = getAction('unload', formattedTime);
+                              pushToDataLayer(action, label);
+                              consoleDebugger('75%' + DEBUGGER.push + videoInfo.guid);
+                          }
+                      }
+                  });
+                  function getCurrentVideo() {
+                      api.get('kulus', function (kulus) {
+                          videoInfo = videos[kulus[0].guid];
+                      });
+                  }
+              });
+          });
+
+          consoleDebugger(videos);
+
+          function initVideosQumu(video) {
+              videos[video.guid] = video;
+              videos[video.guid].percents = calculatePercentsVideo(video);
+              videos[video.guid].isLiveStream = isLiveStream();
+              var formattedTime = formatTime(videoInfo.duration);
+              videos[video.guid].label = isLiveStream() ? video.guid + ' | ' + video.title : video.guid + ' | ' + video.title + ' | ' + formattedTime;
+          }
+
+          function calculatePercentsVideo(video) {
+              var durationMs = video.duration;
+              var quarterMs = durationMs * 0.25;
+              var halfMs = durationMs * 0.5;
+              var toQuarterMs = durationMs * 0.75;
+              var totalMs = durationMs;
+              var percents = {
+                  'quarter': {
+                      value: quarterMs,
+                      hit: true
+                  },
+                  'half': {
+                      value: halfMs,
+                      hit: true
+                  },
+                  'toQuarter': {
+                      value: toQuarterMs,
+                      hit: true
+                  },
+                  'total': {
+                      value: totalMs,
+                      hit: true
+                  }
+              }
+              return percents;
+          }
+
+          function getAction(action, time) {
+              var names = {
+                  'load': ACTIONS.media + ' ' + ACTIONS['begin'],
+                  'play': ACTIONS.media + ' ' + ACTIONS['play'],
+                  'pause': ACTIONS.media + ' ' + ACTIONS['pause'],
+                  'ended': ACTIONS.media + ' ' + ACTIONS['ended'],
+                  'unload': ACTIONS.media + ' ' + time + ' ' + ACTIONS['minutes_watching'],
+                  'timeupdate': ACTIONS.media + ' ' + time + ' ' + ACTIONS['milestone_passed']
+              };
+              return ACTIONS.qumu + ' ' + names[action];
+          };
+
+          function isLiveStream() {
+              if (jQuery("div [guid=" + videoInfo.guid + "]").closest("#live-stream").length > 0) {
+                  return true;
+              }
+              return false;
+          }
+
+          function formatTime(ms) {
+              var seconds = ms / 1000;
+              var hours = parseInt(seconds / 3600);
+              hours = ("0" + hours).slice(-2);
+              seconds = seconds % 3600;
+              var minutes = parseInt(seconds / 60);
+              minutes = ("0" + minutes).slice(-2);
+              seconds = seconds % 60;
+              seconds = seconds.toFixed();
+              seconds = ("0" + seconds).slice(-2);
+              return (hours + ':' + minutes + ':' + seconds);
+          }
+
+          function pushToDataLayer(action, label) {
+              window.dataLayer.push({
+                  event: 'videoEvent',
+                  videoAction: action,
+                  videoLabel: label || videoInfo.label
+              });
+          }
+      } else {
+          tryCounter++;
+          consoleDebugger('jQuery not loaded in page. try: ' + tryCounter);
+          if (tryCounter >= 10) {
+              clearInterval(qumuTimer);
+              consoleDebugger('stop qumu tagging');
+          }
       }
-
-      api.get('kulus', function(kulus) {
-        videoInfo = kulus[0];
-      });
-
-      initQumu(videoInfo);
-
-      api.bind('load', function() {
-        action = 'qumu media begin';
-        pushToDataLayer(action, label);
-      });
-
-      api.bind('play', function() {
-        action = 'qumu media play';
-        if (videoInfo.IMCurrentTime > 500) {
-          pushToDataLayer(action, label);
-        }
-      });
-
-      api.bind('pause', function() {
-        action = 'qumu media pause';
-        var timeToFinish = videoInfo.duration - videoInfo.IMCurrentTime;
-        if (timeToFinish > 500) {
-          pushToDataLayer(action, label);
-        }
-      });
-
-      api.bind('ended', function() {
-        action = 'qumu media complete';
-        pushToDataLayer(action, label);
-      });
-
-      api.bind('unload', function() {
-        var time = formatTime(videoInfo.IMCurrentTime);
-        action = 'qumu ' + time + ' minutes watching';
-        pushToDataLayer(action, label);
-      });
-
-      api.bind('timeupdate', function(currentTime) {
-        var time = parseInt(currentTime);
-        videoInfo.IMCurrentTime = currentTime;
-        if (videoInfo.isLiveStream != true) {
-          if (imPercents.quarter.hit && (time > imPercents.quarter.value && time < imPercents.half.value)) {
-            imPercents.quarter.hit = false;
-            action = 'qumu 25% milestone passed';
-            pushToDataLayer(action, label);
+      function consoleDebugger(message) {
+          if(sessionStorage.getItem('imDebug')){
+              console.log('%c 🎥 Qumu: ', 'background: #0077C8; color: #FFFFFF; font-weight: bold; font-size: 12px; padding: 2px; vertical-align: middle', message);
           }
-          if (imPercents.half.hit && (time > imPercents.half.value && time < imPercents.toQuarter.value)) {
-            imPercents.half.hit = false;
-            action = 'qumu 50% milestone passed';
-            pushToDataLayer(action, label);
-          }
-          if (imPercents.toQuarter.hit && (time > imPercents.toQuarter.value && time < imPercents.total.value)) {
-            imPercents.toQuarter.hit = false;
-            action = 'qumu 75% milestone passed';
-            pushToDataLayer(action, label);
-          }
-        } else {
-          var remainder = time % 60000;
-          if (time > 500 && remainder < 250) {
-            var formattedTime = formatTime(videoInfo.IMCurrentTime);
-            action = 'qumu ' + formattedTime + ' minutes watching';
-		        pushToDataLayer(action, label);
-          }
-        }
-      })
-    });
-  });
-
-  function initQumu(videoInfo) {
-    calculatePercents();
-    videoInfo.initTime = 0;
-    videoInfo.isLiveStream = isLiveStream();
-    var formattedTime = formatTime(videoInfo.duration);
-    label = isLiveStream() ? videoInfo.guid + ' | ' + videoInfo.title : videoInfo.guid + ' | ' + videoInfo.title + ' | ' + formattedTime;
-  }
-
-  function calculatePercents() {
-    var durationMs = videoInfo.duration;
-    var quarterMs = durationMs * 0.25;
-    var halfMs = durationMs * 0.5;
-    var toQuarterMs = durationMs * 0.75;
-    var totalMs = durationMs;
-    imPercents = {
-      'quarter': {
-        value: quarterMs,
-        hit: true
-      },
-      'half': {
-        value: halfMs,
-        hit: true
-      },
-      'toQuarter': {
-        value: toQuarterMs,
-        hit: true
-      },
-      'total': {
-        value: totalMs,
-        hit: true
       }
-    }
-  }
-
-  function isLiveStream() {
-    if (jQuery("div [guid=" + videoInfo.guid + "]").closest("#live-stream").length > 0) {
-      return true;
-    }
-    return false;
-  }
-
-  function formatTime(ms) {
-    var seconds = ms / 1000;
-    var hours = parseInt(seconds / 3600);
-    hours = ("0" + hours).slice(-2);
-    seconds = seconds % 3600;
-    var minutes = parseInt(seconds / 60);
-    minutes = ("0" + minutes).slice(-2);
-    seconds = seconds % 60;
-    seconds = seconds.toFixed();
-    seconds = ("0" + seconds).slice(-2);
-    return (hours + ':' + minutes + ':' + seconds);
-  }
-
-  function pushToDataLayer(action,label) {
-    window.dataLayer.push({
-      event: 'videoEvent',
-      videoAction: action,
-      videoLabel: label
-    });
   }
 })();
